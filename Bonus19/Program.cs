48543bc56448254d3878184c91918b1465242214
Clayton <clayton.cox@gmail.com>
2018-10-26 08:00:56 -0400

    added comments

--- Bonus19/Program.cs
+++ Bonus19/Program.cs
@@ -19,18 +19,21 @@ namespace Bonus19
             Console.WriteLine("Welcome to Guenther's Market\n");
             Console.WriteLine("{0,15}", "Menu");
 
+            //init dictionary and init other arrays for card
             int numberOfItems;
             Dictionary<string, double> menuOfItems = new Dictionary<string, double>();
             AddingItems(menuOfItems);
             numberOfItems = menuOfItems.Count;
             ArrayList storeItems = new ArrayList();
             ArrayList storePrice = new ArrayList();
+
             foreach(KeyValuePair<string,double> d in menuOfItems)
             {
                 storeItems.Add(d.Key.ToString());
                 storePrice.Add(d.Value.ToString());
             }
-
+            //prints menu of all items  in a formated way. and ask for inital user input
+            //calls the other methods
             while (contYN)
             {
                 Console.WriteLine("{0,-20}{1,0}", "Item", "Price");
@@ -50,11 +53,13 @@ namespace Bonus19
                 Console.Write("Would you like to order anything else? ");
                 contYN = Continue(Console.ReadLine(), contYN);
             }
+            //calls order compelte method
             OrderComplete();
             Console.ReadLine();
 
         }
         
+        //finds the average of the array price 
         public static double AverageItems()
         {
             double avg = 0;
@@ -67,7 +72,9 @@ namespace Bonus19
             Console.WriteLine("The average price of all items in cart is: ${0}", avg);
             return avg;
         }
-        
+
+        //highest cost goes through the array and find thes highest price in cart,
+        //matches the lowest index of lowest item and prints.
         public static double HighestCost()
         {
             double tempDoub = 0;
@@ -86,6 +93,8 @@ namespace Bonus19
             return tempDoub;
         
         }
+        //lowest cost goes through the array and find thes lowest price in cart,
+        //matches the lowest index of lowest item and prints.
         public static double LowestCost()
         {
             double tempDoub = HighestCost();
@@ -105,6 +114,8 @@ namespace Bonus19
 
         }
 
+        //goes through the dictonary list for the user input if its true it adds the 
+        //user input to array and the value of the dictonay to array price
         public static void AddingToCart(string selection, Dictionary<string, 
             double> menuOfItems)
         {
@@ -126,6 +137,8 @@ namespace Bonus19
                 }
             }
         }
+
+        //user continue method
         public static bool Continue(string userCont, bool temp)
         {
             temp = true;
@@ -153,6 +166,8 @@ namespace Bonus19
             return false;
         }
 
+        //order complete method writes all items in users cart and displays average
+        //highest and lowest
         public static void OrderComplete()
         {
             Console.Clear();
@@ -167,6 +182,7 @@ namespace Bonus19
             LowestCost();
         }
 
+        //adding items to dictonary
         public static void AddingItems(Dictionary<string, double> menuOfItems)
         {
             menuOfItems.Add("apple", 0.99);
